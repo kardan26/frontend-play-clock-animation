fa6be914f4a7a37bffb25865dce59dc35ade97d9
Daniel Karwowski <karwowskid26@gmail.com>
2019-06-02 23:52:25 +0200

    Fix issues with webpack build for local env after adding gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -16,44 +16,47 @@ const html_webpack_plugin = new HtmlWebpackPlugin({
 const mini_css_extract_plugin = new MiniCssExtractPlugin({
     filename: "[name].css",
 })
-module.exports = {
-    entry: ['./src/js/index.js', './src/scss/index.scss'],
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: 'bundle.js',
-        publicPath: "/frontend-play-clock-animation/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextWebpackPlugin.extract({
-                    use: [{
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'sass-loader'
-                        }
-                    ]
-                })
+
+module.exports = env => {
+    return {
+        entry: ['./src/js/index.js', './src/scss/index.scss'],
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: 'bundle.js',
+            publicPath: (env && env.NODE_ENV == 'github') ? '/frontend-play-clock-animation/' : ''
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: ExtractTextWebpackPlugin.extract({
+                        use: [{
+                                loader: 'css-loader'
+                            },
+                            {
+                                loader: 'sass-loader'
+                            }
+                        ]
+                    })
+                }
+        
+            ]
+        },
+        plugins: [
+            html_webpack_plugin,
+            mini_css_extract_plugin,
+            new ExtractTextWebpackPlugin({ filename: 'index.css' }),
+        ],
+        devServer:{
+            port:3000,
+            overlay: true,
+            historyApiFallback: true,
+            open:true,
+            hot:false,
+            watchOptions: {
+              poll: true
             }
-    
-        ]
-    },
-    plugins: [
-        html_webpack_plugin,
-        mini_css_extract_plugin,
-        new ExtractTextWebpackPlugin({ filename: 'index.css' }),
-    ],
-    devServer:{
-		port:3000,
-		overlay: true,
-		historyApiFallback: true,
-		open:true,
-		hot:false,
-		watchOptions: {
-	      poll: true
-	    }
-	},
-    watch: true
+        },
+        watch: true
+    }
 };
